4d255eaf7e7a7627e1a6f7b0a4dbffcd33704237
maeaooe <141057582+maeaooe@users.noreply.github.com>
2023-09-08 15:52:51 +0800

    Update formation.js

--- js/formation.js
+++ js/formation.js
@@ -1,1 +1,6 @@
-
+const select1 = document.getElementById("name1");
+const img1 = document.getElementById("img1");
+select1.addEventListener("change", event => {
+     const name = event.target.value;
+     img1.src = "/img/" + name + ".png";
+});
